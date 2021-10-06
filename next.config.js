59a74df5dc421dc3e0062216eeff64881d78e306
Stackbit Code Editor <projects@stackbit.com>
2021-10-06 12:33:45 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -14,7 +14,6 @@ module.exports = {
         // Instead, the src/pages/[...slug].js uses the "withRemoteDataUpdates"
         // function to update the content on the page without refreshing the
         // whole page
-        config.plugins.push(new webpack.WatchIgnorePlugin([[/\/content\//]]));
         return config;
     }
 };
